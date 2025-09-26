7056f36b0a7f7c62f35884f01661ab48a225c728
Zagros <zagros@xalantico.com>
2025-09-26 02:51:16 +0200

    lexia publish 1.2

--- lexia/__init__.py
+++ lexia/__init__.py
@@ -6,7 +6,7 @@ Clean, minimal package for Lexia platform integration.
 Contains only essential components for communication.
 """
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 from .models import ChatResponse, ChatMessage, Variable, Memory
 from .response_handler import create_success_response
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read_requirements():
 
 setup(
     name="lexia",
-    version="1.1.0",
+    version="1.2.0",
     author="Lexia Team",
     author_email="support@lexiaplatform.com",
     description="Clean, minimal package for Lexia platform integration",
